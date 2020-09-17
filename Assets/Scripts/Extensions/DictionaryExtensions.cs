05230a7c910576139c945135a4890a1c63650bc0
Quixatocs <quixatocs@gmail.com>
2020-09-17 08:42:07 +0100

    added GUIcontroller and stubs of UI statemachine control, also added IGUIState interface

--- Assets/Scripts/Extensions/DictionaryExtensions.cs
+++ Assets/Scripts/Extensions/DictionaryExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 
 public static class DictionaryExtensions {
-    /// <summary>
+    
     /// Will add the value to the dictionary, overriding it if it already exists.
-    /// </summary>
-    /// <typeparam name="T">The generic type parameter</typeparam>
     public static void SetValue<TKey,TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value) {
         if(dictionary.ContainsKey(key)) {
             dictionary[key] = value;
---
+++ Assets/Scripts/UI/GUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GUIController : MonoBehaviour {
+
+    private Coroutine stateCompleteCheck;
+    private IGUIState currentState;
+
+    private void SetState(IGUIState nextState) {
+        
+    }
+
+    private IEnumerator CheckComplete() {
+        yield return null;
+    }
+}
