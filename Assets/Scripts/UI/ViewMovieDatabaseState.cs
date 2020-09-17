3798b8595fb0629f27fe899512bbc4867b56c8b5
Quixatocs <quixatocs@gmail.com>
2020-09-17 09:54:04 +0100

    adding in IGUIState implementations for intended UI screens

---
+++ Assets/Scripts/UI/ViewMovieDatabaseState.cs
@@ -0,0 +1,8 @@
+﻿
+public class ViewMovieDatabaseState : IGUIState {
+    public bool IsComplete { get; }
+    public IGUIState NextState { get; }
+    public void OnEnter() {
+        throw new System.NotImplementedException();
+    }
+}
